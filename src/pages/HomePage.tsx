d3be7b72f874f011c577115828072098d5411252
KalinkinFiz <kalinkin.fiz@gmail.com>
2023-03-20 02:27:19 +0300

    feat: add HomePage

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+import Header from '../components/Header';
+import Cards from '../components/Cards';
+
+import { IBook } from '../models/types';
+import { books } from '../data/data';
+
+class HomePage extends Component<object, { books: IBook[] }> {
+  constructor(props: object) {
+    super(props);
+    this.state = { books: books };
+  }
+
+  handleBooksFetched = (books: IBook[]) => {
+    this.setState({ books: books });
+  };
+
+  render() {
+    return (
+      <>
+        <Header onBooksFetched={this.handleBooksFetched} hideSearch={false} />
+        <div>
+          <h1>Home Page</h1>
+        </div>
+        <Cards books={this.state.books} />
+      </>
+    );
+  }
+}
+
+export default HomePage;
