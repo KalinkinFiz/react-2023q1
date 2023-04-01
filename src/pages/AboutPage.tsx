cbd278caf7fc768caba4fd7dee6814454d1b1957
KalinkinFiz <kalinkin.fiz@gmail.com>
2023-04-01 10:48:59 +0300

    feat: replace Page404 and AboutPage class component in functional component

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
+import React, { FC } from 'react';
 
 import { Header } from '../components/Header';
 
-export class AboutPage extends Component {
-  render() {
-    return (
-      <>
-        <Header hideSearch={true} />
-        <div>
-          <h1>About us</h1>
-          <p>Information will come later</p>
-        </div>
-      </>
-    );
-  }
-}
+export const AboutPage: FC = () => {
+  return (
+    <>
+      <Header hideSearch={true} />
+      <div>
+        <h1>About us</h1>
+        <p>Information will come later</p>
+      </div>
+    </>
+  );
+};
