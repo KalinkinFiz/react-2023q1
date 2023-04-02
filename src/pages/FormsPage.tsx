5e12233f46b257937cedb8462cc080d76803a345
KalinkinFiz <kalinkin.fiz@gmail.com>
2023-04-02 17:19:39 +0300

    feat: replace FormPage class component in functional component

--- src/pages/FormsPage.tsx
+++ src/pages/FormsPage.tsx
@@ -1,44 +1,42 @@
-import React, { Component } from 'react';
+import React, { FC, useState } from 'react';
 
-import { Header } from '../components/Header';
-import { Form } from '../components/Form';
-import { FormCard } from '../components/FormCard';
+import Header from '../components/Header';
+import Form from '../components/Form';
+import FormCard from '../components/FormCard';
 
 import { IForm } from '../models/types';
 
-export class FormsPage extends Component<Record<string, never>, { forms: IForm[] }> {
-  constructor(props: Record<string, never>) {
-    super(props);
+interface IFormState {
+  forms: IForm[];
+}
 
-    const forms = localStorage.getItem('forms') || '[]';
-    this.state = {
-      forms: JSON.parse(forms),
-    };
-    this.setForm = this.setForm.bind(this);
-  }
+export const FormsPage: FC = () => {
+  const forms = localStorage.getItem('forms') || '[]';
 
-  setForm() {
+  const [state, setState] = useState<IFormState>({
+    forms: JSON.parse(forms),
+  });
+
+  const setForm = () => {
     const forms = localStorage.getItem('forms');
-    forms && this.setState({ forms: JSON.parse(forms) });
-  }
+    forms && setState({ forms: JSON.parse(forms) });
+  };
 
-  render() {
-    return (
-      <>
-        <Header hideSearch={true} />
+  return (
+    <>
+      <Header hideSearch={true} />
+      <div>
+        <h1>Forms Page</h1>
+        <Form setForm={setForm} />
         <div>
-          <h1>Forms Page</h1>
-          <Form setForm={this.setForm} />
-          <div>
-            <p>Submited forms</p>
-            <div className="form-card">
-              {this.state.forms.map((form, i) => {
-                return <FormCard formData={form} formNumber={i} key={i} />;
-              })}
-            </div>
+          <p>Submited forms</p>
+          <div className="form-card">
+            {state.forms.map((form, i) => {
+              return <FormCard formData={form} formNumber={i} key={i} />;
+            })}
           </div>
         </div>
-      </>
-    );
-  }
-}
+      </div>
+    </>
+  );
+};
