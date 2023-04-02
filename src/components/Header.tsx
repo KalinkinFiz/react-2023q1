78a46a2be196a2a5e9c4ba346d675f5c5b583850
KalinkinFiz <kalinkin.fiz@gmail.com>
2023-04-02 17:19:08 +0300

    feat: replace Header class component in functional component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,27 @@
-import React, { Component } from 'react';
+import React, { FC } from 'react';
 import { Link } from 'react-router-dom';
 
 import { IBook } from '../models/types';
-import { SearchBar } from './SearchBar';
+import SearchBar from './SearchBar';
 
-export class Header extends Component<
-  { onBooksFetched?: (books: IBook[]) => void; hideSearch: boolean },
-  object
-> {
-  render() {
-    return (
-      <>
-        <header className="header-main">
-          <div className="header-links">
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-            <Link to="/form">Forms</Link>
-          </div>
-          {!this.props.hideSearch && <SearchBar onBooksFetched={this.props.onBooksFetched} />}
-        </header>
-      </>
-    );
-  }
+interface IHeaderProps {
+  onBooksFetched?: (books: IBook[]) => void;
+  hideSearch: boolean;
 }
+
+const Header: FC<IHeaderProps> = (props) => {
+  return (
+    <>
+      <header className="header-main">
+        <div className="header-links">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/form">Forms</Link>
+        </div>
+        {!props.hideSearch && <SearchBar onBooksFetched={props.onBooksFetched} />}
+      </header>
+    </>
+  );
+};
+
+export default Header;
